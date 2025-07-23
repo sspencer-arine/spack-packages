9be6223901e753ab0829fcad71a80faa1a544e1a
Todd Gamblin <tgamblin@llnl.gov>
2025-07-23 12:25:02 -0700

    curl: add 8.14.1 and 8.15.0
    8.11.1 has CVEs and triggers scanners. Update to the latest 8.14.1 and 8.15.0 releases.
    
    Signed-off-by: Todd Gamblin <tgamblin@llnl.gov>

--- repos/spack_repo/builtin/packages/curl/package.py
+++ repos/spack_repo/builtin/packages/curl/package.py
@@ -29,9 +29,15 @@ class Curl(NMakePackage, AutotoolsPackage):
 
     license("curl")
 
-    version("8.11.1", sha256="e9773ad1dfa21aedbfe8e1ef24c9478fa780b1b3d4f763c98dd04629b5e43485")
+    version("8.15.0", sha256="699a6d2192322792c88088576cff5fe188452e6ea71e82ca74409f07ecc62563")
+    version("8.14.1", sha256="5760ed3c1a6aac68793fc502114f35c3e088e8cd5c084c2d044abdf646ee48fb")
 
     # Deprecated versions due to CVEs
+    version(
+        "8.11.1",
+        sha256="e9773ad1dfa21aedbfe8e1ef24c9478fa780b1b3d4f763c98dd04629b5e43485",
+        deprecated=True,
+    )
     version(
         "8.10.1",
         sha256="3763cd97aae41dcf41950d23e87ae23b2edb2ce3a5b0cf678af058c391b6ae31",
